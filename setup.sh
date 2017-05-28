3923cf1372f1bc3437c81535b3fb4b8b29d1da78
asinha94 <anuraagsinha@hotmail.com>
2017-05-27 20:50:19 -0700

    fixed forloop

--- setup.sh
+++ setup.sh
@@ -5,8 +5,10 @@ FILES=".emacs .emacs.d .bash_aliases .bash_profile .tmux.conf"
 CURRENT_DIR=`pwd`
 
 # Symmolic links
-for f in $FILES
-ln -sf $CURRENT_DIR/$f ~/$f
+for f in $FILES; do
+    ln -sf $CURRENT_DIR/$f ~/$f
+done
 
 # Install the extra stuff that I need
+sudo apt-get update
 sudo apt-get install -y tmux guake emacs arp-scan
