aa589b22e163dfca021f11b05b2efdddcde173bc
asinha94 <asinha94@gmail.com>
2018-02-10 02:05:31 -0800

    added ohmyzsh stuff

--- setup.sh
+++ setup.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Install the extra stuff that I need
+sudo apt-get update
+sudo apt-get install -y tmux guake emacs arp-scan zsh fonts-powerline
+
+# Install oh my zsh
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
 # Add any newer doftiles to the list
 FILES=".emacs .emacs.d .bash_aliases .bash_profile .tmux.conf .zshrc"
 CURRENT_DIR=`pwd`
@@ -9,6 +16,7 @@ for f in $FILES; do
     ln -sf $CURRENT_DIR/$f ~/$f
 done
 
-# Install the extra stuff that I need
-sudo apt-get update
-sudo apt-get install -y tmux guake emacs arp-scan zsh fonts-powerline
+# Copy agnoster modified theme
+ln -s agnoster.zsh-theme ~/.oh-my-zsh/themes/
+
+
