b2406783d9ff97aa73671a3a16eda2abf1ea235e
Netskrt Systems Inc <netskrt@blockchain-01.dev.netskrt.org>
2018-11-06 16:51:27 -0800

    added fix for link

--- setup.sh
+++ setup.sh
@@ -1,22 +1,14 @@
 #!/bin/bash
 
-# Install the extra stuff that I need
-sudo apt-get update
-sudo apt-get install -y tmux guake emacs arp-scan zsh fonts-powerline
-
-# Install oh my zsh
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-
 # Add any newer doftiles to the list
-FILES=".emacs .emacs.d .bash_aliases .bash_profile .tmux.conf .zshrc"
+FILES=".emacs .emacs.d .bash_aliases"
 CURRENT_DIR=`pwd`
 
 # Symmolic links
+cd $HOME
 for f in $FILES; do
     ln -sf $CURRENT_DIR/$f ~/$f
 done
 
-# Copy agnoster modified theme
-cp -f agnoster.zsh-theme ~/.oh-my-zsh/themes/
 
 
