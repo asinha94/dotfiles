1d6a0c60824eb8d62537936ca8276ec70234bbd1
vagrant <vagrant@vagrant.vm>
2018-11-20 18:52:54 +0000

    added elpy and added back other stuff

--- .emacs
+++ .emacs
@@ -34,6 +34,13 @@
 (show-paren-mode t)
 (setq show-paren-style 'expression)
 
+;; elpy
+(package-initialize)
+(elpy-enable)
+(setq elpy-rpc-backend "jedi")
+(setq elpy-rpc-python-command "python3")
+(define-key elpy-mode-map (kbd "M-,") 'pop-tag-mark)
+
 ;; Auto-Complete and dependencies
 (require 'auto-complete-config)
 (ac-config-default)
--- setup.sh
+++ setup.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 
+# Install the extra stuff that I need
+sudo apt-get update
+sudo apt-get install -y tmux guake emacs arp-scan zsh fonts-powerline python3 python-pip python3-pip
+PIP_PACKS="rope jedi flake8 autopep8 yapf"
+pip install $PIP_PACKS
+pip3 install $PIP_PACKS
+
+
+# Install oh my zsh
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
 # Add any newer doftiles to the list
-FILES=".emacs .emacs.d .bash_aliases"
+FILES=".emacs .emacs.d .bash_aliases .bash_profile .tmux.conf .zshrc"
 CURRENT_DIR=`pwd`
 
 # Symmolic links
-cd $HOME
 for f in $FILES; do
     ln -sf $CURRENT_DIR/$f ~/$f
 done
 
+# Copy agnoster modified theme
+cp -f agnoster.zsh-theme ~/.oh-my-zsh/themes/
 
 
